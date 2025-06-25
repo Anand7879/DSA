027378364ea38e103f9ef962c85006e58678917a
Anand7879 <Anand7879@users.noreply.github.com>
2025-06-25 22:54:10 +0530

    Day8

--- LeetCodeDaily/Day8.java
+++ LeetCodeDaily/Day8.java
@@ -19,21 +19,83 @@ public class Day8 {
         System.out.println(kthSmallestProduct(nums1, nums2, k));
     }
 
-     public static long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+    //  public static long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
         
-        ArrayList<Long> list = new ArrayList<>();
+    //     ArrayList<Long> list = new ArrayList<>();
 
-        for(int i = 0; i<nums1.length; i++)
-        {
-            for(int j = 0; j<nums2.length; j++)
-            {
-                long temp = 1L * nums1[i] * nums2[j];  // cast to long to prevent overflow
-                list.add(temp);   
+    //     for(int i = 0; i<nums1.length; i++)
+    //     {
+    //         for(int j = 0; j<nums2.length; j++)
+    //         {
+    //             long temp = 1L * nums1[i] * nums2[j];  // cast to long to prevent overflow
+    //             list.add(temp);   
+    //         }
+    //     }
+
+    //     Collections.sort(list);
+
+    //    return list.get((int)k - 1);
+    // }
+
+
+
+
+
+
+// Optimise Solution
+
+     public static long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = -1_000_000_0000L;  // Minimum possible product
+        long right = 1_000_000_0000L;  // Maximum possible product
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countPairs(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
 
-        Collections.sort(list);
-
-       return list.get((int)k - 1);
+        return left;
     }
+
+    private static long countPairs(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+
+        for (int a : nums1) {
+            if (a == 0) {
+                if (target >= 0) {
+                    count += nums2.length; // all zero products <= any positive target
+                }
+                // if target < 0, 0 > target is false, so count += 0
+            } else if (a > 0) {
+                // Find max j such that a * nums2[j] <= target
+                int l = 0, r = nums2.length;
+                while (l < r) {
+                    int m = l + (r - l) / 2;
+                    if ((long)a * nums2[m] <= target) {
+                        l = m + 1;
+                    } else {
+                        r = m;
+                    }
+                }
+                count += l;
+            } else { // a < 0
+                // Find min j such that a * nums2[j] <= target
+                int l = 0, r = nums2.length;
+                while (l < r) {
+                    int m = l + (r - l) / 2;
+                    if ((long)a * nums2[m] <= target) {
+                        r = m;
+                    } else {
+                        l = m + 1;
+                    }
+                }
+                count += nums2.length - l;
+            }
+        }
+
+        return count;
+    }   
 }
