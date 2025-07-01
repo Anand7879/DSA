3e5f5c1f01c2e38fa892a416c402191c10bc80a1
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-01 20:39:50 +0530

    Day14

---
+++ LeetCodeDaily/Day14.java
@@ -0,0 +1,19 @@
+// Date 1st july 2025
+// Day Tuesday
+
+public class Day14 {
+    public static void main(String[] args) {
+        String word = "abbcccc";
+        System.out.println(possibleStringCount(word));
+    }
+    public static int possibleStringCount(String word) {
+        int ans = 1;  // count the "no long‑press" case
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
