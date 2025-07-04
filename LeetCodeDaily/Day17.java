ede3f9acb603828016a2872b56e3024a8a24483a
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-04 21:25:28 +0530

    Day17

--- LeetCodeDaily/Day17.java
+++ LeetCodeDaily/Day17.java
@@ -3,12 +3,17 @@
 
 public class Day17 {
     public static void main(String[] args) {
-       int k = 5;
-       int[] operations = {0,0,0};
-       System.out.println(kthCharacter(k, operations));
+    //    int k = 5;
+    //    int[] operations = {0,0,0};
+
+      int k = 10;
+       int[] operations = {0,1,0,1};
+    //    System.out.println(kthCharacter(k, operations));
+    kthCharacter(k, operations);
+
     }
 
-    public static char kthCharacter(long k, int[] operations) {
+    public static void kthCharacter(long k, int[] operations) {
         StringBuilder str = new StringBuilder();
         str.append("a");
 
@@ -18,10 +23,16 @@ public class Day17 {
             {
                 str.append('a');
             }else{
-                
+                for(int j = 0; j<str.length(); j++)
+                {
+                    char nextchar =  (char) (str.charAt(i)+1);
+                    str.append(nextchar);
+                }
             }
 
         }
 
+        System.out.println(str);
+            
     }
 }
