4b6904049ea9ff199218514b136527682949fc0f
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-05 21:14:10 +0530

    Day18

---
+++ LeetCodeDaily/Day18.java
@@ -0,0 +1,19 @@
+// Date 5th july 2025
+// Day Saturday
+
+public class Day18 {
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,4};
+        System.out.println(findLucky(arr));
+    }
+
+    public static int findLucky(int[] arr) {
+        
+        int luckyInt = 0;
+
+        for(int i = 0; i<arr.length; i++)
+        {
+            
+        }
+    }
+}
