d61a1698a90f618b58940a681752d71f6b93a635
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-05 21:20:21 +0530

    Day18

--- LeetCodeDaily/Day18.java
+++ LeetCodeDaily/Day18.java
@@ -1,19 +1,26 @@
 // Date 5th july 2025
 // Day Saturday
-
+import java.util.*;
 public class Day18 {
     public static void main(String[] args) {
         int[] arr = {2,2,3,4};
         System.out.println(findLucky(arr));
     }
+         public static int findLucky(int[] arr) {
+             HashMap<Integer,Integer> freqMap = new HashMap<>();
+             for (int i : arr) {
+                freqMap.put(i, freqMap.getOrDefault(i,0)+1);
+             }
+        int largestLucky = -1;
 
-    public static int findLucky(int[] arr) {
-        
-        int luckyInt = 0;
-
-        for(int i = 0; i<arr.length; i++)
-        {
-            
+        for (int key : freqMap.keySet()) {
+            if(key==freqMap.get(key))
+            {
+                largestLucky = Math.max(largestLucky, key);
+            }
         }
+
+        return largestLucky;
     }
+
 }
