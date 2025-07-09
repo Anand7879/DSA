2150338fcfe91d50849458cff53d6718336f38ab
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-09 22:08:11 +0530

    Day22

--- LeetCodeDaily/Day22.java
+++ LeetCodeDaily/Day22.java
@@ -3,7 +3,9 @@
 
 public class Day22 {
     public static void main(String[] args) {
-        
+       int eventTime = 5, k = 1;
+       int[] startTime = {1,3},endTime = {2,5};
+       System.out.println(maxFreeTime(eventTime, k, startTime, endTime));
     }
 
     public static int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
