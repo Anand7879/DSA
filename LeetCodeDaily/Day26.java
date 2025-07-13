daf981dda2b2b666e2f0796ede2cd6575c3e2dcd
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-13 23:13:13 +0530

    Day26

--- LeetCodeDaily/Day26.java
+++ LeetCodeDaily/Day26.java
@@ -1,7 +1,25 @@
 //Date 13th july 2025
 // Day Sunday
+import java.util.*;
 public class Day26 {
     public static void main(String[] args) {
         
     }
+
+    public static int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i = 0, j = 0, matches = 0;
+        while (i < players.length && j < trainers.length) {
+            if (players[i] <= trainers[j]) {
+                matches++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return matches;
+    }
 }
+
