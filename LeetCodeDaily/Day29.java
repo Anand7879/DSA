9304978614b1d84f55937bb7ad19b04bf3c6c4e0
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-16 23:53:31 +0530

    Day29

--- LeetCodeDaily/Day29.java
+++ LeetCodeDaily/Day29.java
@@ -5,4 +5,31 @@ public class Day29 {
     public static void main(String[] args) {
         
     }
+
+    public int maximumLength(int[] nums) {
+        // Strategy 1: All adjacent pairs have the same parity
+        int sameParityLength = 1;
+        int lastSame = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i] % 2) == (lastSame % 2)) {
+                sameParityLength++;
+                lastSame = nums[i];
+            }
+        }
+
+        // Strategy 2: All adjacent pairs have different parity
+        int altParityLength = 1;
+        int lastAlt = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i] % 2) != (lastAlt % 2)) {
+                altParityLength++;
+                lastAlt = nums[i];
+            }
+        }
+
+        return Math.max(sameParityLength, altParityLength);
+    }
 }
+
