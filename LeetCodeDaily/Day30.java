58cfffa4d6522ee7eba2ce648bd3e98086d6d46f
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-17 22:54:28 +0530

    Day30

--- LeetCodeDaily/Day30.java
+++ LeetCodeDaily/Day30.java
@@ -3,7 +3,9 @@
 
 public class Day30 {
     public static void main(String[] args) {
-        
+        int[] nums = {1,2,3,4,5};
+        int k = 2;
+        System.out.println(maximumLength(nums, k));
     }
 
     public static int maximumLength(int[] nums, int k) {
