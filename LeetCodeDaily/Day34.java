df40e39b4cc243b92be5ddff39ab00c003acabfc
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-21 22:39:50 +0530

    DAy34

--- LeetCodeDaily/Day34.java
+++ LeetCodeDaily/Day34.java
@@ -5,9 +5,30 @@ public class Day34 {
     public static void main(String[] args) {
        String s = "aaabaaaa";
     System.out.println(makeFancyString(s));
+    // makeFancyString(s);
     }
-    
+
     public static String makeFancyString(String s) {
         
+        char ch = s.charAt(0);
+        int count = 1;
+        String str ="";
+        str=str+ch;
+    
+        for(int i = 1; i<s.length(); i++)
+        {
+             if(s.charAt(i)==ch){
+                count++;
+             }else{
+                ch = s.charAt(i);
+                count = 1;
+             }
+
+             if(count < 3){
+                 str+=ch;
+             }
+        }
+
+      return str;
     }
 }
