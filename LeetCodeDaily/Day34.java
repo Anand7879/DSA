44ccad73e0efffe1256680a5c3ec0c175865da4f
Anand7879 <Anand7879@users.noreply.github.com>
2025-07-21 21:20:13 +0530

    Day34

---
+++ LeetCodeDaily/Day34.java
@@ -0,0 +1,13 @@
+// Date: 21st July 2025
+// Day: Monday
+
+public class Day34 {
+    public static void main(String[] args) {
+       String s = "aaabaaaa";
+    System.out.println(makeFancyString(s));
+    }
+    
+    public static String makeFancyString(String s) {
+        
+    }
+}
