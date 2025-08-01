22e82e2d7e40a2af57c8dc69a36656dbb898b438
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-01 23:04:02 +0530

    Day45

---
+++ LeetCodeDaily/Day45.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Day45 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int numRows = 5;
+        List<List<Integer>> result = sol.generate(numRows);
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+    
+}
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        if (numRows <= 0) return triangle;
+
+        triangle.add(new ArrayList<>(Arrays.asList(1))); // first row
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev = triangle.get(i - 1);
+            List<Integer> curr = new ArrayList<>();
+            curr.add(1); // first element
+
+            // interior elements
+            for (int j = 1; j < i; j++) {
+                curr.add(prev.get(j - 1) + prev.get(j));
+            }
+
+            curr.add(1); // last element
+            triangle.add(curr);
+        }
+
+        return triangle;
+    }
+
+    // For testing
+}
+
