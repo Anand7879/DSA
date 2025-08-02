71c9c21afe4043d16177bca359e2b16b7c10dadf
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-02 21:23:11 +0530

    Day46

---
+++ LeetCodeDaily/Day46.java
@@ -0,0 +1,12 @@
+//Date: 2nd August 2025
+// Day: Saturday
+
+public class Day46 {
+    public static void main(String[] args) {
+        
+    }
+
+     public long minCost(int[] basket1, int[] basket2) {
+        
+    }
+}
