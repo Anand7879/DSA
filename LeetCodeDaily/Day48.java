b6b378b72c6ef02cf7c50ba973ee0d4fbcca1004
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-04 23:49:17 +0530

    Day48

--- LeetCodeDaily/Day48.java
+++ LeetCodeDaily/Day48.java
@@ -2,4 +2,38 @@ public class Day48 {
     public static void main(String[] args) {
         
     }
+
+    public int totalFruit(int[] fruits) {
+        if (fruits == null || fruits.length == 0) return 0;
+
+        int type1 = -1, type2 = -1; // the two fruit types in window
+        int lastFruit = -1;         // most recent fruit type
+        int lastFruitCount = 0;     // how many times lastFruit appeared consecutively at the end
+        int currentWindow = 0;      // current valid window size
+        int max = 0;
+
+        for (int f : fruits) {
+            if (f == type1 || f == type2) {
+                // extend window
+                currentWindow++;
+            } else {
+                // new fruit type enters: window shrinks to the last contiguous block of lastFruit plus this new one
+                currentWindow = lastFruitCount + 1;
+                type1 = lastFruit;
+                type2 = f;
+            }
+
+            if (f == lastFruit) {
+                lastFruitCount++;
+            } else {
+                lastFruitCount = 1;
+                lastFruit = f;
+            }
+
+            max = Math.max(max, currentWindow);
+        }
+
+        return max;
+    }
 }
+
