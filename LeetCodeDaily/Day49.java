b88c9aa9c25445111e3eb20a87e009d714f16d62
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-05 23:28:52 +0530

    Day49

---
+++ LeetCodeDaily/Day49.java
@@ -0,0 +1,28 @@
+//Date: 5th August 2025
+// Day: Tuesday
+
+public class Day49 {
+    public static void main(String[] args) {
+        
+    }
+}
+public class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[n];
+        int unplaced = 0;
+        
+        for (int fruit : fruits) {
+            boolean placed = false;
+            for (int i = 0; i < n; i++) {
+                if (!used[i] && baskets[i] >= fruit) {
+                    used[i] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) unplaced++;
+        }
+        return unplaced;
+    }
+}
