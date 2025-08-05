450fc0184b05d68d3a980a41d731d576c177bc0f
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-05 23:30:23 +0530

    Day49

--- LeetCodeDaily/Day49.java
+++ LeetCodeDaily/Day49.java
@@ -3,7 +3,10 @@
 
 public class Day49 {
     public static void main(String[] args) {
-        
+        int[] fruits = {4, 2, 5};
+int[] baskets = {3, 5, 4};
+System.out.println(numOfUnplacedFruits(fruits, baskets));  // outputs 1
+
     }
     public static int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         int n = fruits.length;
