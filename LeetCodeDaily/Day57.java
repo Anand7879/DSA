7010d598625c6a061095f30348c91333e110af2f
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-13 21:09:12 +0530

    Day57

--- LeetCodeDaily/Day57.java
+++ LeetCodeDaily/Day57.java
@@ -2,4 +2,9 @@ public class Day57 {
     public static void main(String[] args) {
         
     }
+      public boolean isPowerOfThreeOptimal(int n) {
+        // 3^19 = 1162261467 is the largest power of 3 within int range
+        // If n is a power of 3, then 3^19 should be divisible by n
+        return n > 0 && 1162261467 % n == 0;
+    }
 }
