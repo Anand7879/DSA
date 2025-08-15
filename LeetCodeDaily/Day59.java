8b55377b79874eb168e0433be78f2dbef994d34f
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-15 23:01:29 +0530

    Day59

--- LeetCodeDaily/Day59.java
+++ LeetCodeDaily/Day59.java
@@ -1,10 +1,4 @@
 public class Day59 {
-     public boolean isPowerOfFourModulo(int n) {
-        // A positive integer is a power of 4 if and only if:
-        // 1. It's a power of 2
-        // 2. When divided by 3, remainder is 1
-        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
-    }
     
     // Test method
     public static void main(String[] args) {
@@ -17,12 +11,15 @@ public class Day59 {
         System.out.println("n\tMath\tBitwise\tIterative\tModulo");
         
         for (int n : testCases) {
-            boolean math = sol.isPowerOfFour(n);
-            boolean bitwise = sol.isPowerOfFourBitwise(n);
-            boolean iterative = sol.isPowerOfFourIterative(n);
-            boolean modulo = sol.isPowerOfFourModulo(n);
+            boolean modulo = isPowerOfFourModulo(n);
             
-            System.out.printf("%d\t%b\t%b\t%b\t\t%b%n", n, math, bitwise, iterative, modulo);
+            System.out.printf("%b%n", n,modulo);
         }
     }
+    public static boolean isPowerOfFourModulo(int n) {
+       // A positive integer is a power of 4 if and only if:
+       // 1. It's a power of 2
+       // 2. When divided by 3, remainder is 1
+       return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
+   }
 }
