bedaec568c31e756a2feb4aae799e54677b9fc55
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-19 23:46:44 +0530

    Day63

--- LeetCodeDaily/Day63.java
+++ LeetCodeDaily/Day63.java
@@ -2,4 +2,20 @@ public class Day63 {
     public static void main(String[] args) {
         
     }
+ public static long zeroFilledSubarray(int[] nums) {
+        long result = 0;
+        long consecutiveZeros = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                consecutiveZeros++;
+                result += consecutiveZeros;
+            } else {
+                consecutiveZeros = 0;
+            }
+        }
+        
+        return result;
+    }
+
 }
