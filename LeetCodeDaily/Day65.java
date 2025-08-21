044016c2c9f5c42734d05a787fadd74d1b8d5dae
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-21 22:00:59 +0530

    Day65

--- LeetCodeDaily/Day65.java
+++ LeetCodeDaily/Day65.java
@@ -5,4 +5,35 @@ public class Day65 {
     public static void main(String[] args) {
         
     }
+
 }
+
+ int m = mat.length;
+        int n = mat[0].length;
+        int result = 0;
+        
+        // For each row, calculate heights of consecutive 1s ending at current row
+        int[] heights = new int[n];
+        
+        for (int i = 0; i < m; i++) {
+            // Update heights array for current row
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            
+            // For each column as starting point, count submatrices
+            for (int j = 0; j < n; j++) {
+                int minHeight = heights[j];
+                // Extend to the right and count submatrices
+                for (int k = j; k < n && heights[k] > 0; k++) {
+                    minHeight = Math.min(minHeight, heights[k]);
+                    result += minHeight;
+                }
+            }
+        }
+        
+        return result;
