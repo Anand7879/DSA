0a3dfa69650e9fbe0e4b1948aec9b13c162991f5
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-24 21:56:56 +0530

    Day68

--- LeetCodeDaily/Day68.java
+++ LeetCodeDaily/Day68.java
@@ -3,3 +3,20 @@ public class Day68 {
         
     }
 }
+class Solution {
+    public int longestSubarray(int[] nums) {
+         int maxLen = 0;
+        int left = 0;
+        int zeroIndex = -1;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                left = zeroIndex + 1;
+                zeroIndex = right;
+            }
+            maxLen = Math.max(maxLen, right - left);
+        }
+        
+        return maxLen;
+    }
+}
